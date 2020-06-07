54e29492e248bc2a3e2ee5a9c81d1e79b3ee80f2
Ezandro Bueno <57026708+ezbueno@users.noreply.github.com>
2020-06-07 05:24:17 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -2,8 +2,14 @@ const express = require("express")
 
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db.js")
+
 // configurar pasta public
 server.use(express.static("public"))
+
+// habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // utilizando template engine
 const nunjucks = require("nunjucks")
@@ -22,11 +28,61 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+    // req.query: Query Strings da nossa url
+    //console.log(req.query)
+
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+    // req.body: O corpo do nosso formulário
+    // console.log(req.body)
+
+    // inserir dados no banco de dados
+    const query = `INSERT INT places (image, name, address, address2, state, city, items)
+                   VALUES (?, ?, ?, ?, ?, ?, ?);`
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            //return res.send("Erro no cadastro!")
+            return res.render("create-point.html", { saved: false })
+
+        }
+
+        console.log("Cadastrado com sucesso!")
+        console.log(this)
+
+        return res.sender("create-point.html", { saved: true } )
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
+
 server.get("/search-results", (req, res) => {
-    return res.render("search-results.html")
+
+    // pegar os dados do banco de dados
+    db.all(`SELECT * FROM places`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        // mostrar a página HTML com os dados do banco de dados
+        return res.render("search-results.html", { places: rows, total: total })
+    }) 
 })
 
 
